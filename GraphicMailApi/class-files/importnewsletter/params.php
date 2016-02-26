eaa4b91f71dc5ed7645a7625fcbeaa238ca757bb
Proceed <steve@prcd.co>
2016-02-26 18:14:44 +0000

    Added function - import_newsletter

---
+++ GraphicMailApi/class-files/importnewsletter/params.php
@@ -0,0 +1,9 @@
+<?php
+
+$this->createQueryParam('htmlurl',       'HtmlURL', true);
+$this->createQueryParam('type',          'NewExisting', true);
+$this->createQueryParam('id',            'NewsletterID');
+$this->createQueryParam('name',          'NewsletterName');
+$this->createQueryParam('replaceimages', 'ReplaceImages');
+$this->createQueryParam('replacelinks',  'ReplaceLinks');
+$this->createQueryParam('texturl',       'TextURL');
