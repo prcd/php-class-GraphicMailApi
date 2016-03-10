15591e2ebe2bb3fe8771e503786868b3e68e6dbe
Proceed <steve@prcd.co>
2016-03-10 12:07:22 +0000

    Added function - delete_from_data_set

---
+++ GraphicMailApi/class-files/deletefromdataset/handler.php
@@ -0,0 +1,22 @@
+<?php
+
+if ($this->result == '') {
+	$rtn['status']  = 'ERR';
+	$rtn['message'] = 'No response from GraphicMail';
+}
+else if ($this->result == '0|Emailaddress does not exist in this dataset. ') {
+	$rtn['status'] = 'OK';
+	$rtn['data']['email_deleted'] = '0';
+}
+else if (substr($this->result, 0, 2) == '0|') {
+	$rtn['status']  = 'ERR';
+	$rtn['message'] = substr($this->result,2);
+}
+else if (substr($this->result, 0, 2) == '1|') {
+	$rtn['status'] = 'OK';
+	$rtn['data']['email_deleted'] = '1';
+}
+else {
+	$rtn['status']  = 'ERR';
+	$rtn['message'] = 'Unexpected response from GraphicMail';
+}
