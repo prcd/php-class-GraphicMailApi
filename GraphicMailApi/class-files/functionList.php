1896d6ecb528dd26a612801d09700a254230f1ff
Proceed <steve@prcd.co>
2016-03-10 11:54:33 +0000

    Added function - delete_from_data_set

--- GraphicMailApi/class-files/functionList.php
+++ GraphicMailApi/class-files/functionList.php
@@ -6,6 +6,7 @@
 $functionList = array(
 	'copymailinglist'             => 'post_copy_mailinglist',
 	'createmailinglist'           => 'post_create_mailinglist',
+	'deletefromdataset'           => 'post_delete_from_dataset',
 	'deleteemail'                 => 'post_delete_emailaddress',
 	'deletestatusfrommailinglist' => 'post_delete_status_from_mailinglist',
 	'getcredits'                  => 'get_all_credits',
