003fe7ea28c004e7062fd445baae8c525afba9e8
Proceed <steve@prcd.co>
2016-03-17 15:07:33 +0000

    Added function - create_sub_account

---
+++ GraphicMailApi/class-files/createsubaccount/params.php
@@ -0,0 +1,29 @@
+<?php
+
+$this->createQueryParam('password', 'NewPassword', true);
+$this->createQueryParam('username', 'NewUsername', true);
+$this->createQueryParam('address1', 'Address1');
+$this->createQueryParam('address2', 'Address2');
+$this->createQueryParam('apipassword', 'APIPassword');
+$this->createQueryParam('buycredits', 'BuyCredits');
+$this->createQueryParam('contactnumber', 'ContactNumber');
+$this->createQueryParam('countryid', 'CountryID');
+$this->createQueryParam('draganddropenabled', 'DragAndDropEnabled');
+$this->createQueryParam('emailsnotviewable', 'EmailsNotViewable');
+$this->createQueryParam('name', 'Name');
+$this->createQueryParam('normalemailcredits', 'NormalEmailCredits');
+$this->createQueryParam('organisation', 'Organization');
+$this->createQueryParam('postcode', 'PostalCode');
+$this->createQueryParam('replenishcredits', 'ReplenishCredits');
+$this->createQueryParam('selecttemplates', 'SelectTemplates');
+$this->createQueryParam('shareapiips', 'ShareApiIPs');
+$this->createQueryParam('sharedatasets', 'ShareDatasets');
+$this->createQueryParam('sharefooter', 'ShareFooter');
+$this->createQueryParam('shareimages', 'ShareImages');
+$this->createQueryParam('sharelists', 'ShareLists');
+$this->createQueryParam('sharetemplates', 'ShareTemplates');
+$this->createQueryParam('showprofile', 'ShowProfile');
+$this->createQueryParam('smsenabled', 'SMSEnabled');
+$this->createQueryParam('statsonly', 'StatsOnly');
+$this->createQueryParam('surname', 'Surname');
+$this->createQueryParam('websiteurl', 'WebsiteURL');
